3de1974c29b9129a8f476abbd9bdb68b2794f4e7
gopikrishnapothula <gopikrishnap1999@gmail.com>
2023-12-10 16:40:17 +0530

    deploy

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.common.WhiteNoiseMiddleware', # deploy
+    'whitenoise.middleware.WhiteNoiseMiddleware', # deploy
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
